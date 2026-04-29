186322032ae77e3f5487e95dd90b240bcb405710
Pedro Delfino <pedro.delfino.dev@outlook.com>
2026-04-28 22:07:51 -0300

    add: finalizando test exceptions

--- tests/test_exceptions.py
+++ tests/test_exceptions.py
@@ -1,4 +1,6 @@
 from playwright.sync_api import expect
+
+from conftest import setup
 from pages.home_page import HomePage
 
 
@@ -10,7 +12,7 @@ def test_NoSuchElementException(setup):
 
     page.get_by_role("button", name="Add").click()
     expect(page.get_by_text("Row 2 was added")).to_be_visible()
-    expect(page.get_by_text("Row 2 Save Edit Remove")).to_be_visible()
+    expect(page.get_by_text("Row 2", exact=True)).to_be_visible(timeout=10000)
 
 def test_ElementNotInteractableException(setup):
     page = setup
@@ -35,3 +37,23 @@ def test_InvalidElementStateException(setup):
     page.get_by_role("textbox").fill("Hot Dog")
     page.get_by_role("button", name="Save").click()
     expect(page.get_by_text("Row 1 was saved")).to_be_visible()
+
+def test_StaleElementReferenceException(setup):
+    page = setup
+
+    main_page = HomePage(page)
+    main_page.select_Test_Exceptions()
+
+    expect(page.locator("#instructions")).to_contain_text("Push “Add” button to add another row")
+    page.get_by_role("button", name="Add").click()
+    expect(page.locator("#instructions")).not_to_have_text("Push “Add” button to add another row")
+
+def test_TimeoutException(setup):
+    page = setup
+
+    main_page = HomePage(page)
+    main_page.select_Test_Exceptions()
+
+    page.get_by_role("button", name="Add").click()
+    page.wait_for_timeout(timeout=3000)
+    expect(page.get_by_text("Row 2", exact=True)).not_to_be_visible()
